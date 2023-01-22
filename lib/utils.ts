4170af1d338b5f456a936d571611728ae8b76ea2
Valentin Galfre <valentin@vrio.com>
2023-01-22 19:30:41 -0300

    Adding utilities file

---
+++ lib/utils.ts
@@ -0,0 +1,54 @@
+import clsx from 'clsx';
+export { clsx as cx };
+
+export const isExternalLink = (url: string): boolean => {
+  if (typeof url === 'string') {
+    return url.startsWith('http');
+  }
+
+  return false;
+};
+
+export const isEmptyLink = (url: string): boolean => {
+  if (typeof url === 'string') {
+    return url === '';
+  }
+
+  return false;
+};
+
+interface formatDateProps {
+  timestamp?: number | string | null;
+  year?: 'numeric' | '2-digit';
+  month?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow';
+  day?: 'numeric' | '2-digit';
+}
+
+export const formatDate = ({
+  timestamp = null,
+  year = 'numeric',
+  month = 'long',
+  day = 'numeric',
+}: formatDateProps) => {
+  const date = timestamp ? new Date(timestamp) : new Date();
+  const formatted = date.toLocaleDateString('en-us', {
+    year,
+    month,
+    day,
+  });
+
+  return { formatted };
+};
+
+export const prefersReducedMotion = (() => {
+  let shouldReduceMotion: boolean | undefined = undefined;
+
+  return () => {
+    if (shouldReduceMotion === undefined && typeof window !== 'undefined') {
+      const mediaQuery = matchMedia('(prefers-reduced-motion: reduce)');
+      shouldReduceMotion = !mediaQuery || mediaQuery.matches;
+    }
+
+    return shouldReduceMotion;
+  };
+})();
