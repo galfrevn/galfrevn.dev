1b966ec0c9bba0046267d7c580139f6e49aecba3
Valentin Galfre <valentin@vrio.com>
2023-01-22 19:35:09 -0300

    Installing Poppins font via @next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@/styles/global.css';
+
+import { ThemeProvider } from 'next-themes';
+import { Poppins } from '@next/font/google';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const poppinsFont = Poppins({
+  variable: '--inter-font',
+  weight: ['400', '500', '600', '700', '800'],
+});
+
+const FontStyles = () => (
+  <style jsx global>{`
+    :root {
+      --font-inter: ${poppinsFont.style.fontFamily};
+    }
+  `}</style>
+);
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout =
+    Component.getLayout ||
+    ((page) => (
+      <ThemeProvider defaultTheme="system">
+        <FontStyles />
+        {page}
+      </ThemeProvider>
+    ));
+
+  return getLayout(<Component {...pageProps} />);
 }
