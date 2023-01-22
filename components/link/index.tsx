73fe55a4dc0df6781d5e7a3841b4c41dcc8b7d96
Valentin Galfre <valentin@vrio.com>
2023-01-22 19:58:36 -0300

    Creating shared-link component

---
+++ components/link/index.tsx
@@ -0,0 +1,36 @@
+import NextLink from 'next/link';
+import { cx, isExternalLink, isEmptyLink } from 'lib/utils';
+
+import styles from 'components/link/link.module.css';
+
+interface LinkProps {
+  to?: string;
+  href?: string;
+  className?: string;
+  underline?: boolean;
+  children: React.ReactNode;
+}
+
+export const Link = ({ to, href, className, underline, children }: LinkProps) => {
+  const url = to ?? (href as string);
+
+  const classes = cx(className, {
+    [styles.root]: href,
+    [styles.underline]: underline,
+  });
+
+  if (isExternalLink(url))
+    return (
+      <a className={classes} href={url} rel="noopener noreferrer" target="_blank">
+        {children}
+      </a>
+    );
+
+  if (isEmptyLink(url)) return <div className={classes}>{children}</div>;
+
+  return (
+    <NextLink href={url} className={classes}>
+      {children}
+    </NextLink>
+  );
+};
