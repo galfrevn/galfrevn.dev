0219df35456ee0405dc099853b763c57de809f0f
Valentin Galfre <valentin@vrio.com>
2023-01-22 21:05:44 -0300

    Adding card component

---
+++ components/card/index.tsx
@@ -0,0 +1,83 @@
+import Image from 'next/image';
+
+import { Link } from 'components/link';
+import { isExternalLink } from 'lib/utils';
+
+import styles from 'components/card/card.module.css';
+
+interface CardProps {
+  title?: string;
+  description?: string;
+  link: string;
+  category?: string;
+  icon?: {
+    image: string;
+    background?: string;
+    invert?: boolean;
+  };
+}
+
+const List = ({ children }: { children?: React.ReactNode }) => {
+  return <div className={styles.root}>{children}</div>;
+};
+
+const Item = ({ icon, title, description, link, category }: CardProps) => {
+  const imageSrc = `https://res.cloudinary.com/galfrevn/image/upload/c_scale,h_48/v1674429584/galfrevndev/${icon?.image}`;
+
+  return (
+    <Link to={link} className={styles.item}>
+      <div className={icon ? styles.header : ''}>
+        {icon && (
+          <div
+            className={styles.image}
+            style={
+              {
+                '--i-bg': icon?.background ?? 'var(--accents-4)',
+                '--i-filter': icon?.invert ? 'var(--invert)' : 'none',
+              } as React.CSSProperties
+            }
+          >
+            <Image src={imageSrc} alt={title as string} width={48} height={48} />
+          </div>
+        )}
+        <div>
+          <h3>{title}</h3>
+          <span>{category}</span>
+        </div>
+      </div>
+      <p>{description}</p>
+
+      {isExternalLink(link) ? (
+        <div className={styles.external}>
+          <svg
+            width={14}
+            height={14}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke="var(--accents-2)"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"
+            />
+            <path
+              stroke="var(--accents-2)"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
+            />
+          </svg>
+        </div>
+      ) : null}
+    </Link>
+  );
+};
+
+export const CardLink = {
+  List,
+  Item,
+};
