7a85c1047e15674a6243d7041b36769c412404e5
Valentin Galfre <valentin@vrio.com>
2023-01-22 21:06:10 -0300

    Adding stack constants file

---
+++ data/stack.ts
@@ -0,0 +1,196 @@
+export default [
+  {
+    name: '1.1.1.1',
+    description: 'Privacy for your browsing',
+    category: 'Software',
+    url: 'https://1.1.1.1/',
+    icon: {
+      image: 'stack/internet_safer_efrrun.png',
+      background: 'rgba(239, 105, 114, 0.1)',
+    },
+  },
+  {
+    name: '1Password',
+    description: 'Manage all your passwords',
+    category: 'Software',
+    url: 'https://1password.com/',
+    icon: {
+      image: 'stack/1password_z4iawf.png',
+    },
+  },
+  {
+    name: 'Blender',
+    description: '3D modeling and animating software',
+    category: 'Tools',
+    url: 'https://www.blender.org/',
+    icon: {
+      image: 'stack/blender_f2uy8f.png',
+      background: 'rgba(234, 118, 0, 0.06)',
+    },
+  },
+  {
+    name: 'Figma',
+    description: 'Design & Prototype Software',
+    category: 'Tools',
+    url: 'https://figma.com/',
+    icon: {
+      image: 'stack/figma_ahejiq.png',
+      background: 'rgba(162, 89, 255, 0.07)',
+    },
+  },
+  {
+    name: 'GitHub CLI',
+    description: 'Command Line Interface for GitHub',
+    category: 'Software',
+    url: 'https://cli.github.com/',
+    icon: {
+      image: 'stack/github_cli_yute2o.png',
+      invert: true,
+    },
+  },
+  {
+    name: 'Loom',
+    description: 'Video Messaging for Work',
+    category: 'Software',
+    url: 'https://www.loom.com/',
+    icon: {
+      image: 'stack/loom_sg5oai.png',
+      background: 'rgba(98, 93, 245, 0.07)',
+    },
+  },
+  {
+    name: 'MagicaVoxel',
+    description: '3D Modeling Soft',
+    category: 'Tools',
+    url: 'https://www.voxelmade.com/magicavoxel/',
+    icon: {
+      image: 'stack/magicavoxel_lesyew.png',
+    },
+  },
+  {
+    name: 'Next.js',
+    description: 'Server-side render framework',
+    category: 'Tools',
+    url: 'https://nextjs.org/',
+    icon: {
+      image: 'stack/nextjs_fhcsrk.png',
+      invert: true,
+    },
+  },
+  {
+    name: 'Notion',
+    description: 'Take notes and create to-dos',
+    category: 'Software',
+    url: 'https://www.notion.so/',
+    icon: {
+      image: 'stack/notion_hkjbbr.png',
+    },
+  },
+  {
+    name: 'Oh My Posh',
+    description: 'Prompt a theme engine for any shell',
+    category: 'Tools',
+    url: 'https://ohmyposh.dev/',
+    icon: {
+      image: 'stack/windows_terminal_lbmol0.png',
+    },
+  },
+  {
+    name: 'Tailwind CSS',
+    description: 'CSS framework focusing on utility-first',
+    category: 'Tools',
+    url: 'https://tailwindcss.com/',
+    icon: {
+      image: 'stack/tailwindcss_esfwqb.png',
+      background: 'rgba(56, 189, 248, 0.07)',
+    },
+  },
+  {
+    name: 'VSCode',
+    description: 'Code editor for devs',
+    category: 'Tools',
+    url: 'https://code.visualstudio.com/',
+    icon: {
+      image: 'stack/vscode_xevi8v.png',
+      background: 'rgba(34, 167, 242, 0.07)',
+    },
+  },
+  {
+    name: 'Vercel',
+    description: 'Platform for deploying web pages',
+    category: 'Tools',
+    url: 'https://vercel.com/',
+    icon: {
+      image: 'stack/vercel_yezosg.png',
+      invert: true,
+    },
+  },
+  {
+    name: 'Windows Terminal',
+    description: 'Terminal emulator for Windows',
+    category: 'Software',
+    url: 'https://docs.microsoft.com/en-us/windows/terminal/install',
+    icon: {
+      image: 'stack/windows_terminal_lbmol0.png',
+    },
+  },
+  {
+    name: 'PowerToys',
+    description: 'Utilities to customize Windows',
+    category: 'Tools',
+    url: 'https://learn.microsoft.com/en-us/windows/powertoys/',
+    icon: {
+      image: 'stack/powertoys_oajtf5.png',
+    },
+  },
+  {
+    name: 'Fluent Search',
+    description: 'Find fast what you need',
+    category: 'Software',
+    url: 'https://fluentsearch.net/',
+    icon: {
+      image: 'stack/fluent_search_veveav.png',
+      invert: true,
+    },
+  },
+  {
+    name: 'Prisma',
+    description: 'Next-Gen ORM for Node.js & TS',
+    category: 'Tools',
+    url: 'https://www.prisma.io/',
+    icon: {
+      image: 'stack/prisma_yxlxax.png',
+      invert: true,
+    },
+  },
+  {
+    name: 'PlanetScale',
+    description: 'Cloud-native DB scaling for all',
+    category: 'Tools',
+    url: 'https://planetscale.com/',
+    icon: {
+      image: 'stack/planetscale_ftrvwl.png',
+      invert: true,
+    },
+  },
+  {
+    name: 'Spotify',
+    description: 'Music streaming service',
+    category: 'Software',
+    url: 'https://spotify.com/',
+    icon: {
+      image: 'stack/spotify_evbhpg.png',
+      background: 'rgba(30, 215, 96, 0.07)',
+    },
+  },
+  {
+    name: 'Cloudflare',
+    description: 'Cloudflare speeds & protects sites',
+    category: 'Software',
+    url: 'https://cloudflare.com/',
+    icon: {
+      image: 'stack/cloudflare_ppdgs9.png',
+      background: 'rgba(244, 128, 31, 0.06)',
+    },
+  },
+];
