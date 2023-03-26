b02e458ff01a932eebf98b7d49a1f6c316cb1eb9
Valentin Galfre <valentin@vrio.com>
2023-03-26 16:14:18 -0300

    Removed invalid character in about me description

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <AboutHero />
       <AnimatedImage />
       <p className='text-lg font-normal leading-relaxed text-dark-400 dark:text-dark-200 mb-10'>
-        My name is Valentín Galfré. I am 20 years old, I live in Copenhagen, Denmark, and I'm a
+        My name is Valentín Galfré. I am 20 years old, I live in Copenhagen, Denmark, and I&apos;m a
         self-taught designer and developer. Digital design has been my main focus for many years,
         specifically UI/UX, but I have the past years shifted my focus to frontend development,
         which I have developed a great passion for. I enjoy the constant change in the technologies
